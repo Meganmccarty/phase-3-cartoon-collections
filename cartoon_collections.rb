2111653567215a80c2e4cd6604836297d4f55d9c
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-13 08:40:06 -0400

    commit tree 5568adc287266150fa9cbac630ae27fb6f94257d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+    dwarf_array.each_with_index do |dwarf, index|
+        number = index + 1
+        puts "#{number}. #{dwarf}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# puts roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map do |planeteer_call|
+        "#{planeteer_call.capitalize}!"
+    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# puts summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+
+def long_planeteer_calls(calls)
+    calls.any? do |call|
+        call.length > 4
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(potential_cheese)
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types.find do |cheese|
+        potential_cheese.include?(cheese)
+    end
 end
